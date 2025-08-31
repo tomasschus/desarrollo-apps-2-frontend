5372c3222f9f3543738407995e1fe8ad02b18e7e
Tomas Schuster <ts2525@hotmail.com>
2025-08-31 20:20:51 -0300

    feat: add dayjs for date manipulation and localization support

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,19 @@
+import dayjsObjectSupport from "dayjs/plugin/objectSupport";
+import dayjsRelativeTime from "dayjs/plugin/relativeTime";
+import dayjsUtc from "dayjs/plugin/utc";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import { Provider } from "./components/ui/provider.tsx";
 import { AuthProvider } from "./contexts/auth-context.tsx";
+
+import dayjs from "dayjs";
+import "dayjs/locale/es";
+
+dayjs.locale("es");
+dayjs.extend(dayjsRelativeTime);
+dayjs.extend(dayjsUtc);
+dayjs.extend(dayjsObjectSupport);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
