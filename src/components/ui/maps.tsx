e994c222062cd135cfca02710f1f8213367e3cad
tomas schuster <ts2525@hotmail.com>
2025-09-05 20:42:34 -0300

    feat: fix default markers in production by updating icon URLs in Maps component

--- src/components/ui/maps.tsx
+++ src/components/ui/maps.tsx
@@ -1,4 +1,5 @@
 import { Card, Text } from "@chakra-ui/react";
+import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 import {
   MapContainer,
@@ -8,6 +9,17 @@ import {
   TileLayer,
   type MapContainerProps,
 } from "react-leaflet";
+
+// Fix for default markers in production
+delete (L.Icon.Default.prototype as any)._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png",
+  iconUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png",
+  shadowUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png",
+});
 
 type MapsProps = {
   cardTitle: string;
