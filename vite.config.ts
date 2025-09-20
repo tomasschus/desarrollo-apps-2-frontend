7321d536deb798114d5baad4b07cd639bea8e8aa
Nicolas Diaz <diaz.nicolasandres@gmail.com>
2025-09-20 15:02:30 -0300

    url vercel

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api/v1": {
-        target: "https://desarrollo-apps-2-backend.onrender.com",
+        target: "https://desarrollo-apps2-back-end.vercel.app",
         changeOrigin: true,
         secure: true,
         headers: {
