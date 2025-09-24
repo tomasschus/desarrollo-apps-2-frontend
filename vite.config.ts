73b9a1755d9f04e828965e5628ac8181f2ed14c7
Nicolas Diaz <diaz.nicolasandres@gmail.com>
2025-09-24 14:27:10 -0300

    rollback vite config for test

--- vite.config.ts
+++ vite.config.ts
@@ -5,15 +5,6 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.lottie'],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://desarrollo-apps2-back-end.vercel.app',
-        changeOrigin: true,
-        secure: true,
-      },
-    },
-  },
   build: {
     rollupOptions: {
       output: {
